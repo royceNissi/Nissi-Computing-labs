4380fa5ed40744ef00a064506a3278e9dc43b344
royceNissi <royce.christon@nissiagency.com>
2023-04-02 20:47:12 +0530

    changed contact styling

--- src/pages/FormContactUs.js
+++ src/pages/FormContactUs.js
@@ -11,14 +11,14 @@ export const Form=()=>{
                     <input type="email" placeholder="Email" />
                     <textarea cols="30" rows="5" placeholder="Message">
                     </textarea>
-                    <button>Submit</button>
+                    <button className="submit-button">Submit</button>
                 </div>
                 <div className="address-container">
-                    <h6 className="address-header">Office Address</h6>
+                    <h6 className="sub-heading">Office Address</h6>
                     <p className="address">#74, Patel Ram Reddy Rd, Domlur I Stage, Bengaluru Karnataka, India - 560071</p>
-                    <h6 className="email-header">E-mail</h6>
+                    <h6 className="sub-heading">E-mail</h6>
                     <p className="email-address">info@nissiagency.com</p>
-                    <h6 className="phone-header">Phone</h6> 
+                    <h6 className="sub-heading">Phone</h6> 
                     <p className="phone-number">+91 8088100900</p>
                 </div>
             </div>
